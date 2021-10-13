1ba8f1de24d25effaca624492417b412bf473d7e
melkholy <melkholy@student.42wolfsburg.de>
2021-10-14 01:22:56 +0200

    Ex04 Second commit

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 23:13:41 by coder             #+#    #+#             */
-/*   Updated: 2021/10/13 03:53:52 by coder            ###   ########.fr       */
+/*   Updated: 2021/10/14 01:21:51 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_strlen(char *str)
 	return (count);
 }
 
-int	ft_chdup(char *str)
+int	ft_check(char *str)
 {
 	int	i_count;
 	int	o_count;
@@ -33,6 +33,8 @@ int	ft_chdup(char *str)
 	while (str[o_count] != '\0' && flag == 0)
 	{
 		i_count = 0;
+		if (str[o_count] == '-' || str[o_count] == '+')
+			flag ++;
 		while (str[i_count] != '\0' && flag == 0)
 		{
 			if (i_count != o_count && str[i_count] == str[o_count])
@@ -40,22 +42,6 @@ int	ft_chdup(char *str)
 			i_count ++;
 		}
 		o_count ++;
-	}
-	return (flag);
-}
-
-int	ft_inval(char *str)
-{
-	int	count;
-	int	flag;
-
-	count = 0;
-	flag = 0;
-	while (str[count] != '\0')
-	{
-		if (str[count] == '-' || str[count] == '+')
-			flag ++;
-		count ++;
 	}
 	return (flag);
 }
@@ -74,58 +60,47 @@ void	ft_pos_comput(int num, char *base, int val)
 	}
 }
 
-void	ft_n_comput(int num, char *base, int val)
+void	ft_neg_check(int num, char *base, int val)
 {
-	int	div;
-	int	mod;
-
-	if (num > 0)
+	if (num < val && num >= 0)
 	{
-		div = num / val;
-		ft_n_comput(div, base, val);
-		mod = num % val;
-		val --;
-		write(1, &base[val - mod], 1);
+		write(1, &base[num / val], 1);
+		write(1, &base[num % val], 1);
 	}
-}
-
-void	ft_neg_comput(int num, char *base, int val)
-{
-	if (num == -2147483648)
-		num = (num + 1) * 1;
-	ft_n_comput(num, base, val);
+	else
+		ft_pos_comput(num, base, val);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int	temp;
 	int	n_base;
 
 	n_base = ft_strlen(base);
-	if (n_base < 2 || ft_chdup(base) > 0 ||ft_inval(base) > 0)
+	if (n_base < 2 || ft_check(base) > 0)
 		return ;
-	else if (nbr < n_base && nbr >= 0)
+	else if (nbr == 0)
+		write(1, &base[nbr], 1);
+	else if (nbr < n_base && nbr > 0)
 	{
-		temp = nbr / n_base;
-		write(1, &base[temp], 1);
-		temp = nbr % n_base;
-		write(1, &base[temp], 1);
+		write(1, &base[nbr / n_base], 1);
+		write(1, &base[nbr % n_base], 1);
 	}
 	else if (nbr > 0)
 		ft_pos_comput(nbr, base, n_base);
 	else
 	{
 		nbr *= -1;
-		ft_neg_comput(nbr, base, n_base);
+		write(1, "-", 1);
+		ft_neg_check(nbr, base, n_base);
 	}
 }
 
-int    main(void)
-{
-	int	num;
-	char str[] = "01";
-	
-	num = -75;
-	ft_putnbr_base(num, str);
-	return (0);
-}
+// int	main(void)
+// {
+// 	int		num;
+// 	char	str[] = "0123456789abcdef";
+
+// 	num = 5763;
+// 	ft_putnbr_base(num, str);
+// 	return (0);
+// }
